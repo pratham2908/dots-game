3e5da9122b454a5e5559af1344dc693fe46187a5
Pratham Garg <aggarwal.prathamgarg@gmail.com>
2023-01-17 20:21:17 +0530

    Fixed implementation

--- script.js
+++ script.js
@@ -58,7 +58,12 @@ lines.forEach(line => {
             if (!checkForBox(line, line.parentNode)) {
                 changeTurn();
             } else {
+                sound.pause();
                 sound.play();
+            }
+
+            if (isOver()) {
+                showResult();
             }
         }
     })
@@ -66,29 +71,30 @@ lines.forEach(line => {
 
 function checkForBox(line, parent) {
     let lineNo = Array.from(parent.children).indexOf(line);
-    let color = line.classList.contains('blue') ? "blue" : "red";
+    let color = document.querySelector('.game-turn').innerHTML.includes('1') ? "red" : "blue";
 
     if (parent.classList.contains('row')) {
         let rowNo = Array.from(parent.parentNode.children).indexOf(parent) - 1;
         rowClicked[rowNo][lineNo] = true;
-        let boxesMade = checkCompletionRow(rowNo, lineNo);
-        if (boxesMade) {
+        let count = checkCompletionRow(rowNo, lineNo, color);
+        if (count) {
             if (color == "red") {
-                document.querySelector('.player1-score').innerHTML = Number(document.querySelector('.player1-score').innerHTML) + boxesMade;
+                document.querySelector('.player1-score').innerHTML = Number(document.querySelector('.player1-score').innerHTML) + count;
             } else {
-                document.querySelector('.player2-score').innerHTML = Number(document.querySelector('.player2-score').innerHTML) + boxesMade;
+                document.querySelector('.player2-score').innerHTML = Number(document.querySelector('.player2-score').innerHTML) + count;
+
             }
             return true;
         }
     } else {
         let colNo = Array.from(parent.parentNode.children).indexOf(parent) - 11 - 1;
         colClicked[colNo][lineNo] = true;
-        let boxesMade = checkCompletionCol(colNo, lineNo);
-        if (boxesMade) {
+        let count = checkCompletionCol(colNo, lineNo, color);
+        if (count) {
             if (color == "red") {
-                document.querySelector('.player1-score').innerHTML = Number(document.querySelector('.player1-score').innerHTML) + boxesMade;
+                document.querySelector('.player1-score').innerHTML = Number(document.querySelector('.player1-score').innerHTML) + count;
             } else {
-                document.querySelector('.player2-score').innerHTML = Number(document.querySelector('.player2-score').innerHTML) + boxesMade;
+                document.querySelector('.player2-score').innerHTML = Number(document.querySelector('.player2-score').innerHTML) + count;
             }
             return true;
         }
@@ -96,83 +102,76 @@ function checkForBox(line, parent) {
     }
 }
 
-function checkCompletionCol(colNo, lineNo) {
+function checkCompletionCol(colNo, lineNo, color) {
     let count = 0;
     if (colNo < 15 && colClicked[colNo + 1][lineNo] && rowClicked[lineNo + 1][colNo] && rowClicked[lineNo][colNo]) {
-        const IstClassList = colArr[colNo + 1].children[lineNo].classList.value;
-        const secondClassList = rowArr[lineNo + 1].children[colNo].classList.value;
-        const thirdClassList = rowArr[lineNo].children[colNo].classList.value;
-        const currClassList = colArr[colNo].children[lineNo].classList.value;
-
-        if (IstClassList == secondClassList && secondClassList == thirdClassList && thirdClassList == currClassList) {
-            count++;
-
-        }
+        nameBox(color, lineNo * 55, colNo * 55 + 20);
+        count++;
     }
-
     if (colNo > 0 && colClicked[colNo - 1][lineNo] && rowClicked[lineNo + 1][colNo - 1] && rowClicked[lineNo][colNo - 1]) {
-        const IstClassList = colArr[colNo - 1].children[lineNo].classList.value;
-        const secondClassList = rowArr[lineNo + 1].children[colNo - 1].classList.value;
-        const thirdClassList = rowArr[lineNo].children[colNo - 1].classList.value;
-        const currClassList = colArr[colNo].children[lineNo].classList.value;
-
-
-        if (IstClassList == secondClassList && secondClassList == thirdClassList && thirdClassList == currClassList) {
-            count++;
-        }
+        nameBox(color, lineNo * 55, colNo * 55 - 35);
+        count++;
     }
 
     return count;
+
+
 }
 
-function checkCompletionRow(rowNo, lineNo) {
+function checkCompletionRow(rowNo, lineNo, color) {
     let count = 0;
+
     if (rowNo < 10 && rowClicked[rowNo + 1][lineNo] && colClicked[lineNo + 1][rowNo] && colClicked[lineNo][rowNo]) {
-        const IstClassList = rowArr[rowNo + 1].children[lineNo].classList.value;
-        const secondClassList = colArr[lineNo + 1].children[rowNo].classList.value;
-        const thirdClassList = colArr[lineNo].children[rowNo].classList.value;
-        const currClassList = rowArr[rowNo].children[lineNo].classList.value;
-
-        if (IstClassList == secondClassList && secondClassList == thirdClassList && thirdClassList == currClassList) {
-            count++;
-        }
-
+        nameBox(color, rowNo * 55, lineNo * 55 + 20,);
+        count++;
     }
-
     if (rowNo > 0 && rowClicked[rowNo - 1][lineNo] && colClicked[lineNo + 1][rowNo - 1] && colClicked[lineNo][rowNo - 1]) {
-
-        const IstClassList = rowArr[rowNo - 1].children[lineNo].classList.value;
-        const secondClassList = colArr[lineNo + 1].children[rowNo - 1].classList.value;
-        const thirdClassList = colArr[lineNo].children[rowNo - 1].classList.value;
-        const currClassList = rowArr[rowNo].children[lineNo].classList.value;
-
-
-        if (IstClassList == secondClassList && secondClassList == thirdClassList && thirdClassList == currClassList) {
-            count++;
-        }
-
+        nameBox(color, (rowNo - 1) * 55, lineNo * 55 + 20,);
+        count++;
     }
 
     return count;
+
 }
 
 function changeTurn() {
-    lines.forEach(line => {
-        if (!line.classList.contains('active'))
-            line.classList.toggle('blue');
-    })
-
     document.querySelectorAll('.game-info-item').forEach(item => {
         item.classList.toggle('active');
     })
 
     const turn = document.querySelector('.game-turn');
+    turn.innerHTML = turn.innerHTML.includes('1') ? "Player 2 Turn" : "Player 1 Turn";
+}
 
-    if (turn.innerHTML.includes('1')) {
-        turn.innerHTML = "Player 2 Turn";
-    } else {
-        turn.innerHTML = "Player 1 Turn";
+function nameBox(color, top, left) {
+    const letter = document.createElement("div");
+    letter.classList.add("letter");
+    letter.innerHTML = color == "red" ? "1 " : "2";
+    letter.classList.add(color)
+    letter.style.top = `calc(${top}px)`
+    letter.style.left = `calc(${left}px)`
+    game.appendChild(letter);
+
+}
+
+function isOver() {
+    for (let i = 0; i < lines.length; i++) {
+        if (!lines[i].classList.contains('active')) {
+            return false;
+        }
     }
+
+
+    return true;
+}
+
+function showResult() {
+    const player1Score = document.querySelector('.player1-score').innerHTML;
+    const player2Score = document.querySelector('.player2-score').innerHTML;
+    const winner = player1Score > player2Score ? "Player 1" : "Player 2";
+
+    document.querySelector('.winner').classList.add('active');
+    document.querySelector('.winner-text').innerHTML = `${winner} wins with a score of ${Math.max(player1Score, player2Score)} : ${Math.min(player1Score, player2Score)}`
 }
 
 
